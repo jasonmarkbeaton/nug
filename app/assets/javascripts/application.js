389993e862a0af4ac42a9033c4adbb25e5576ae7
parkerrr <jason.beaton@gmail.com>
2015-03-10 15:52:10 -0400

    Just a bit more styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,15 @@
 //= require_tree .
 
 $(document).ready() {
-  // Headroom //
-  $('#header').Headroom();
+  $('.nav').on('click', function(e) {
+    e.preventDefault();
+    $(this).next().toggleClass('active');
+  });
+
+  $(document).click(function (e) {
+    var containers = $('.menu, .nav');
+    if (!containers.is(e.target) && containers.has(e.target).length === 0){
+        containers.removeClass('active');
+    }
+  });
 }
